54ed3761593f6a0296ac82952fe24003b5ddb1b5
Ainoa Andrés <ainochu50809@outlook.com>
2018-03-25 10:55:33 +0200

    Update docker compose/file

---
+++ subscription-database/script.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE subscription_db;
+
+GRANT ALL PRIVILEGES ON subscription_db.* TO 'adidas_user'@'%' IDENTIFIED BY 'adidas_password';
+GRANT ALL PRIVILEGES ON subscription_db.* TO 'adidas_user'@'%' IDENTIFIED BY 'adidas_password';
+
+USE subscription_db;
+
+CREATE TABLE IF NOT EXISTS subscription (
+  newsletter_id bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  email VARCHAR(100) NOT NULL,
+  first_name VARCHAR(45),
+  agree TINYINT NOT NULL DEFAULT 0,
+  gender ENUM('MALE', 'FEMALE') NULL DEFAULT 'MALE',
+  date_of_birthday DATE NOT NULL,
+  campaign_id bigint(20) NOT NULL,
+  PRIMARY KEY (newsletter_id),
+  UNIQUE INDEX email_UNIQUE (email ASC))
+ENGINE = InnoDB;
